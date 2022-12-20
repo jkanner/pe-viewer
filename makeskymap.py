1b2eba3ad7c9cd6dea74debee5293ed6561fcfde
Jonah Kanner <jkanner@caltech.edu>
2022-12-19 16:23:55 -0800

    fix skymap bug for gwtc1

--- makeskymap.py
+++ makeskymap.py
@@ -13,26 +13,9 @@ def make_skymap(chosenlist):
     aprx_dict = {}
     for ev in chosenlist:
         st.markdown("### Skymap for {0}".format(ev))        
-        # if ev is None: continue
-
-        if int(ev[2:4]) < 18:
-
-            #-- GWTC-1
-            from matplotlib.figure import Figure
-            url = 'https://dcc.ligo.org/public/0157/P1800381/007/{0}_skymap.fits.gz'.format(ev)
-            
-            with lock:
-                fig = Figure()
-                #ax = fig.subplots(subplot_kw={'projection': 'geo aitoff'})
-                ax = fig.subplots(subplot_kw={'projection': 'astro hours mollweide'})    
-                ax.imshow_hpx(url, cmap='cylon')
-                st.pyplot(fig)
-
-        else:
-            # -- GWTC-3
-            data = load_samples(ev)
-            aprx_dict[ev] = st.radio("Select set of samples to use", data.skymap.keys(), key='aprx_'+ev)
-            with lock:
-                fig = data.skymap[aprx_dict[ev]].plot(contour=[50, 90])
-                st.pyplot(fig[0])
+        data = load_samples(ev)
+        aprx_dict[ev] = st.radio("Select set of samples to use", data.skymap.keys(), key='aprx_'+ev)
+        with lock:
+            fig = data.skymap[aprx_dict[ev]].plot(contour=[50, 90])
+            st.pyplot(fig[0])
         
